eb120a69eb24e85aca5802e2aa03923b208b3f43
anandbiju71 <anandbiju71@gmail.com>
2026-04-06 14:55:26 +0530

    checkpoint: update electron/preload.js

--- electron/preload.js
+++ electron/preload.js
@@ -5,6 +5,9 @@ contextBridge.exposeInMainWorld('jarvis', {
   setClickThrough: (enabled) => ipcRenderer.invoke('jarvis:set-clickthrough', enabled),
   toggleHud: () => ipcRenderer.invoke('jarvis:toggle-hud'),
   getBackendStatus: () => ipcRenderer.invoke('jarvis:get-backend-status'),
+  getServiceHealth: () => ipcRenderer.invoke('jarvis:get-service-health'),
+  requestMicrophoneAccess: () => ipcRenderer.invoke('jarvis:request-microphone-access'),
+  getMicrophoneAccess: () => ipcRenderer.invoke('jarvis:get-microphone-access'),
   setCredential: (key, value) => ipcRenderer.invoke('jarvis:set-credential', key, value),
   getCredential: (key) => ipcRenderer.invoke('jarvis:get-credential', key),
   deleteCredential: (key) => ipcRenderer.invoke('jarvis:delete-credential', key),
@@ -17,5 +20,10 @@ contextBridge.exposeInMainWorld('jarvis', {
     const listener = () => callback();
     ipcRenderer.on('jarvis:toggle-focus', listener);
     return () => ipcRenderer.removeListener('jarvis:toggle-focus', listener);
+  },
+  onServiceHealth: (callback) => {
+    const listener = (_event, payload) => callback(payload);
+    ipcRenderer.on('jarvis:service-health', listener);
+    return () => ipcRenderer.removeListener('jarvis:service-health', listener);
   },
 });
